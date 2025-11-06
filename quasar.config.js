4d9f72bde7452a2fbe5c351e30c469b17bd9118d
Rey Harold Matanguihan <143078241+IvnnM@users.noreply.github.com>
2025-11-06 17:20:22 +0800

    fix deployment

--- quasar.config.js
+++ quasar.config.js
@@ -46,7 +46,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      // publicPath: '/',
+      publicPath: '/CIG-Quasar-Practice/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
